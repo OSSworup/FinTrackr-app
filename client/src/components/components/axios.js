712d923f0ff6508b4c36de57d37b46d941272224
Omm Subham Sworup Ojha <ossworupojha@gmail.com>
2025-07-08 16:52:10 +0530

    Update axios.js

--- client/src/components/components/axios.js
+++ client/src/components/components/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000" });
+const api = axios.create({ baseURL: "https://mern-finance-planner.onrender.com" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
